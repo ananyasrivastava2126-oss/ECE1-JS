568ff6747af87b8a902a112e63bcefa0d5b05fbf
Ananya Srivastava <ananyasrivastava2126@gmail.com>
2026-02-25 10:27:49 +0530

    lecture2

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("JavaScript is working");
+ var num = 10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = "Hello";
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num
+console.log(num);
+console.log(typeof(num));
+// const sym1=symbol(4);
+// console.log(sym2);
+// if{sym1==sym2};
+// }
+// else{
+//     console.log("false")
+// }
+// var a=100; var b=13; var c=10; var linebreak="<br>";
+
+
