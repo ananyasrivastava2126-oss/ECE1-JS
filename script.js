1f48873751056fcd76e7e748dc4f6e53b104be11
Ananya Srivastava <ananyasrivastava2126@gmail.com>
2026-03-11 10:28:13 +0530

    Loops and conditional statements

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-console.log("JavaScript is working");
- var num = 10;
-console.log(num);
-console.log(typeof(num));
-num = false;
-console.log(num);
-console.log(typeof(num));
-num = "Hello";
-console.log(num);
-console.log(typeof(num));
-num = null;
-console.log(num);
-console.log(typeof(num));
-num
-console.log(num);
-console.log(typeof(num));
+// console.log("JavaScript is working");
+//  var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
+// num = "Hello";
+// console.log(num);
+// console.log(typeof(num));
+// num = null;
+// console.log(num);
+// console.log(typeof(num));
+// num
+// console.log(num);
+// console.log(typeof(num));
 // const sym1=symbol(4);
 // console.log(sym2);
 // if{sym1==sym2};
@@ -24,3 +24,103 @@ console.log(typeof(num));
 // var a=100; var b=13; var c=10; var linebreak="<br>";
 
 
+// LOOPS
+// 1
+// var count;
+// document.write["staring loop"+"<br/>"];
+// for(count=0;count<10;count++){
+  
+//     document.write("current loop:"+ count);
+//     document.write("<br/>");
+// }
+// document.write("loop stopped");
+
+
+// 2
+// syntax
+// for(key in object){
+//     // code block to be executed
+// }
+// const person={
+//     fname="Ajay";
+//     lname="Singh";
+//     age=25;
+// }
+// for ( let x in person){
+//     console.log("person details: "+x+","+person[x]);
+//     document.write("br/>");
+// }
+
+// 3 While Loop
+
+
+
+
+
+// conditional statement
+// if statement
+// var age=20;
+// if(age>18){
+//     document.write("<b>You are eligible to vote</b>")
+// }
+
+// if else
+//    var age=20;
+// if(age>18){
+//     document.write("<b>You are Eligible to vote</b>")
+// }else{
+//     console.log("You are a Minor");
+// } 
+
+// Switch case
+// var grade ="A";
+// switch(grade){
+//     case'A':document.write("<br>Good job</br>");
+//     break;
+
+//     case'B': document.write("Pretty Good");
+//     break;
+
+//     case'C': document.write("not so good");
+//     break;
+
+//     case'D':document.write("Passed");
+//     break;
+
+//     case'E':document.write("Fail");
+//     break;
+
+// }
+
+// Functions
+// function myF(){
+
+// alert("Hello!,World");
+// }
+
+// function sayHello(nam,age){
+//     document.write(name+"is"+age+"years old");
+// }
+
+// var res=mul(12,30)
+// function mul(x,y)
+// {
+//     return x*y;
+
+// }
+// document.write(res)
+
+// Arrow Function is an annonymous function.
+
+// object
+// var person={
+//     name:john;
+//     Age:30;
+//     isMarried:true;
+//     address : {
+//         street : "Vinay nagar";
+//          FlatNo: 201
+
+//     }
+// };
+// console.log(person);
