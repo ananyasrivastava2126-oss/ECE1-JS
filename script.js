531d56eca60b9e72916a28db3adaa39e00fab735
Ananya Srivastava <ananyasrivastava2126@gmail.com>
2026-03-19 06:31:27 +0530

    loops and conditonal statement

--- script.js
+++ script.js
@@ -1,126 +1,110 @@
-// console.log("JavaScript is working");
-//  var num = 10;
-// console.log(num);
-// console.log(typeof(num));
-// num = false;
-// console.log(num);
-// console.log(typeof(num));
-// num = "Hello";
-// console.log(num);
-// console.log(typeof(num));
-// num = null;
-// console.log(num);
-// console.log(typeof(num));
-// num
-// console.log(num);
-// console.log(typeof(num));
-// const sym1=symbol(4);
-// console.log(sym2);
-// if{sym1==sym2};
-// }
-// else{
-//     console.log("false")
-// }
-// var a=100; var b=13; var c=10; var linebreak="<br>";
+// script.js
 
+// Basic console logs
+console.log("JavaScript is working");
 
-// LOOPS
-// 1
-// var count;
-// document.write["staring loop"+"<br/>"];
-// for(count=0;count<10;count++){
-  
-//     document.write("current loop:"+ count);
-//     document.write("<br/>");
-// }
-// document.write("loop stopped");
+var num = 10;
+console.log(num);
+console.log(typeof(num));
 
+num = false;
+console.log(num);
+console.log(typeof(num));
 
-// 2
-// syntax
-// for(key in object){
-//     // code block to be executed
-// }
-// const person={
-//     fname="Ajay";
-//     lname="Singh";
-//     age=25;
-// }
-// for ( let x in person){
-//     console.log("person details: "+x+","+person[x]);
-//     document.write("br/>");
-// }
+num = "Hello";
+console.log(num);
+console.log(typeof(num));
 
-// 3 While Loop
+num = null;
+console.log(num);
+console.log(typeof(num));
 
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+// ================= LOOPS =================
 
+// 1. FOR LOOP
+console.log("For Loop:");
+for(let count = 0; count < 5; count++){
+    console.log("Count: " + count);
+}
 
+// 2. WHILE LOOP
+console.log("While Loop:");
+let i = 0;
+while(i < 5){
+    console.log("Value: " + i);
+    i++;
+}
 
+// 3. DO-WHILE LOOP
+console.log("Do While Loop:");
+let j = 0;
+do {
+    console.log("Value: " + j);
+    j++;
+} while(j < 5);
+// 4. FOR...IN LOOP
+const person = {
+    fname: "Ajay",
+    lname: "Singh",
+    age: 25
+};
 
-// conditional statement
-// if statement
-// var age=20;
-// if(age>18){
-//     document.write("<b>You are eligible to vote</b>")
-// }
+console.log("For...in Loop:");
+for(let key in person){
+    console.log(key + " : " + person[key]);
+}
+// ================= CONDITIONAL STATEMENTS =================
 
-// if else
-//    var age=20;
-// if(age>18){
-//     document.write("<b>You are Eligible to vote</b>")
-// }else{
-//     console.log("You are a Minor");
-// } 
+// 1. IF STATEMENT
+var age = 20;
+if(age > 18){
+    console.log("You are eligible to vote");
+}
 
-// Switch case
-// var grade ="A";
-// switch(grade){
-//     case'A':document.write("<br>Good job</br>");
-//     break;
+// 2. IF-ELSE
+if(age > 18){
+    console.log("You are Eligible to vote");
+} else {
+    console.log("You are a Minor");
+}
 
-//     case'B': document.write("Pretty Good");
-//     break;
+// 3. IF-ELSE IF-ELSE
+var marks = 75;
+if(marks >= 90){
+    console.log("Grade A");
+} else if(marks >= 75){
+    console.log("Grade B");
+} else if(marks >= 50){
+    console.log("Grade C");
+} else { console.log("Fail");
+}
 
-//     case'C': document.write("not so good");
-//     break;
+// 4. SWITCH CASE
+var grade = "A";
 
-//     case'D':document.write("Passed");
-//     break;
+switch(grade){
+    case 'A':
+        console.log("Good job");
+        break;
 
-//     case'E':document.write("Fail");
-//     break;
+    case 'B':
+        console.log("Pretty Good");
+        break;
 
-// }
+    case 'C':
+        console.log("Not so good");
+        break;
 
-// Functions
-// function myF(){
+    case 'D':
+        console.log("Passed");
+        break;
 
-// alert("Hello!,World");
-// }
-
-// function sayHello(nam,age){
-//     document.write(name+"is"+age+"years old");
-// }
-
-// var res=mul(12,30)
-// function mul(x,y)
-// {
-//     return x*y;
-
-// }
-// document.write(res)
-
-// Arrow Function is an annonymous function.
-
-// object
-// var person={
-//     name:john;
-//     Age:30;
-//     isMarried:true;
-//     address : {
-//         street : "Vinay nagar";
-//          FlatNo: 201
-
-//     }
-// };
-// console.log(person);
+    case 'E':
+        console.log("Fail");
+        break;
+default:
+        console.log("Invalid Grade");
+}
