40c0270ae78d653d46b95e9f39b6875217a4e9a7
Ananya Srivastava <ananyasrivastava2126@gmail.com>
2026-03-21 08:49:19 +0530

    Implemented mouse events

---
+++ script3.js
@@ -0,0 +1,27 @@
+function onClickEvent() {
+    document.getElementById("output").innerText = "Button Clicked";
+}
+
+function onMouseOver() {
+    document.getElementById("output").innerText = "Mouse Over Event";
+}
+
+function onMouseOut() {
+    document.getElementById("output").innerText = "Mouse Out Event";
+}
+
+function onMouseDown() {
+    document.getElementById("output").innerText = "Mouse Button Pressed";
+}
+
+function onMouseUp() {
+    document.getElementById("output").innerText = "Mouse Button Released";
+}
+
+function onMouseMove() {
+    document.getElementById("output").innerText = "Mouse Moving";
+}
+
+function onDoubleClick() {
+    document.getElementById("output").innerText = "Double Clicked";
+}
